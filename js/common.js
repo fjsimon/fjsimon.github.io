b03a68fcb815c0b25923f65a930e06c2c57bff46
fjsimon <fjsimon@users.noreply.github.com>
2015-12-05 20:08:35 +0000

    Update common.js

--- js/common.js
+++ js/common.js
@@ -7,7 +7,7 @@ requirejs.config({
 		jquery:'https://code.jquery.com/jquery-2.1.4.min',
 		bootstrap:'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
 		velocity: 'https://cdn.jsdelivr.net/velocity/1.2.3/velocity.min',
-		velocity-ui: 'https://cdn.jsdelivr.net/velocity/1.2.3/velocity.ui.min',
+		velocityui: 'https://cdn.jsdelivr.net/velocity/1.2.3/velocity.ui.min',
 		vNotifications:'vNotifications',
 		
 	}
