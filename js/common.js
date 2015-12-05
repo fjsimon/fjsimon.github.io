8f848c75b646f08fb6a79455d8bbd27fa5bcd7ea
fjsimon <fjsimon@users.noreply.github.com>
2015-12-05 20:49:32 +0000

    Update common.js
    Added and removed velocity-ui 1.2.3. Velocity 1.2.3 fails !

--- js/common.js
+++ js/common.js
@@ -7,7 +7,6 @@ requirejs.config({
 		jquery:'https://code.jquery.com/jquery-2.1.4.min',
 		bootstrap:'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
 		velocity: 'https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.2/velocity',
-		velocityui: 'https://cdn.jsdelivr.net/velocity/1.2.3/velocity.ui.min',
 		vNotifications:'vNotifications',
 		
 	}
