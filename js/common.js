384e26543155646b4ca7749b3fd013807eaa3a8c
fjsimon <fjsimon@users.noreply.github.com>
2015-12-05 18:35:32 +0000

    Update common.js
    Links to external plugins

--- js/common.js
+++ js/common.js
@@ -2,8 +2,8 @@ requirejs.config({
 	baseUrl:"../js",
 	paths: {
 		jquery:'https://code.jquery.com/jquery-2.1.4.min',
-		bootstrap:'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min',
-		velocity: 'velocity',
+		bootstrap:'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
+		velocity: 'https://cdnjs.cloudflare.com/ajax/libs/velocity/1.2.3/velocity.min',
 		vNotifications:'vNotifications',
 		
 	}
