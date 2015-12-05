387f47548e48b9b1a75ad02fdd309df669c36952
fjsimon <fjsimon@users.noreply.github.com>
2015-12-05 18:59:07 +0000

    Update common.js
    Added dependency to bootstrap

--- js/common.js
+++ js/common.js
@@ -1,5 +1,8 @@
 requirejs.config({
 	baseUrl:"../js",
+	shim : {
+        	"bootstrap" : { "deps" :['jquery'] }
+    	},
 	paths: {
 		jquery:'https://code.jquery.com/jquery-2.1.4.min',
 		bootstrap:'https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min',
