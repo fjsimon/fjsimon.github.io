e2236832064e76cbaa3d5d8333023780c1df33de
fjsimon <fjsimon@users.noreply.github.com>
2015-12-11 21:22:46 +0000

    Update common.js

--- js/common.js
+++ js/common.js
@@ -3,7 +3,9 @@ requirejs.config({
 	shim : {
         	"bootstrap" : { "deps" : ['jquery'] },
         	"velocity" : { "deps" : ['jquery'] },
-	        "velocityui": { "deps" : ['velocity'] }
+	        "velocityui": { "deps" : ['velocity'] }, 
+	        "bootgrid": ["bootstrap"],
+        	"bootgridfa": ["bootgrid"],
     	},
 	paths: {
 		jquery:'https://code.jquery.com/jquery-2.1.4.min',
@@ -11,6 +13,7 @@ requirejs.config({
 		velocity: 'https://cdn.jsdelivr.net/velocity/1.2.2/velocity.min',
 		velocityui: 'https://cdn.jsdelivr.net/velocity/1.2.2/velocity.ui.min',
 		vNotifications:'vNotifications',
-		
+		bootgrid: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-bootgrid/1.3.1/jquery.bootgrid.min',
+		bootgridfa: 'https://cdnjs.cloudflare.com/ajax/libs/jquery-bootgrid/1.3.1/jquery.bootgrid.fa.min',
 	}
 });
